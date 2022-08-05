256778bc726d828ecfe53e115f04e4bb27420f05
Jonathan Bergson <contato@bergson.me>
2022-08-05 19:42:36 -0300

    chore: iniciado workspace

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+  },
+};
